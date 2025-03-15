dbcbf9580ee7cb14317c30ba150cb9399c1feddc
Priyansh Dimri <priyanshd0908@gmail.com>
2025-03-15 14:52:09 +0530

    add post request route in app.js

--- app.js
+++ app.js
@@ -14,7 +14,29 @@ app.get("/", (req, res) => res.render("index", { messages }));
 
 app.get("/new", (req, res) => res.render("new"));
 
-// TODO: Add POST route to add a new message
+app.post("/new", (req, res) => {
+  const { user, text } = req.body;
+
+  if (!user || !text) {
+    return res.status(400).send("Both name and message are required.");
+  }
+
+  const newMessage = {
+    user,
+    text,
+    added: new Date(),
+  };
+
+  messages.push(newMessage);
+
+  fs.writeFileSync(
+    "./messages.json",
+    JSON.stringify(messages, null, 2),
+    "utf-8"
+  );
+
+  res.redirect("/");
+});
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
